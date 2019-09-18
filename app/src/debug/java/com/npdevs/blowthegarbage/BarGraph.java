a8e8b5f70328b3a66ab831b79c58bcc9bdf33cbe
Ashutosh Chitranshi <ashutosh7234@gmail.com>
2019-09-18 17:04:05 +0530

    Added Bar Chart

--- app/src/debug/java/com/npdevs/blowthegarbage/BarGraph.java
+++ app/src/debug/java/com/npdevs/blowthegarbage/BarGraph.java
@@ -1,101 +1,73 @@
 package com.npdevs.blowthegarbage;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Random;
 
 public class BarGraph extends AppCompatActivity {
 
-    BarChart barChart;
-    ArrayList<String> dates;
-    Random random;
-    ArrayList<BarEntry> barEntries;
+    private BarChart barChart;
+    private ArrayList<BarEntry> barEntries;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_graph);
 
-        barChart = (BarChart) findViewById(R.id.bargraph);
+        barChart = findViewById(R.id.bargraph);
+        databaseReference = FirebaseDatabase.getInstance().getReference("garbage-cleaned");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                HashMap<String,Integer> garbage = new HashMap<>();
+                ArrayList<String> dates = new ArrayList<>();
+                for(DataSnapshot post : dataSnapshot.getChildren())
+                {
+                    String date = post.getKey().toString();
+                    date = date.substring(0,4)+'/'+date.substring(4,6)+'/'+date.substring(6);
+                    garbage.put(date,Integer.parseInt(post.getValue().toString()));
+                    dates.add(date);
+                }
+               // Log.e("ashu",garbage.get(dates.get(0))+"");
+                createRandomBarGraph(garbage,dates);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-        createRandomBarGraph("2016/05/05", "2016/05/10");
+            }
+        });
 
     }
 
-    public void createRandomBarGraph(String Date1, String Date2){
+    public void createRandomBarGraph(HashMap<String,Integer> garbage,ArrayList<String> dates){
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
-
-        try {
-            Date date1 = simpleDateFormat.parse(Date1);
-            Date date2 = simpleDateFormat.parse(Date2);
-
-            Calendar mDate1 = Calendar.getInstance();
-            Calendar mDate2 = Calendar.getInstance();
-            mDate1.clear();
-            mDate2.clear();
-
-            mDate1.setTime(date1);
-            mDate2.setTime(date2);
-
-            dates = new ArrayList<>();
-            dates = getList(mDate1,mDate2);
-            HashMap<String, Integer> hashMap = new HashMap<>();
-            for(int i=0;i<dates.size();i++){
-                hashMap.put(dates.get(i),Integer.parseInt(i+1+""));
-            }
             barEntries = new ArrayList<>();
-            float max = 0f;
-            //  float value = 0f;
-            random = new Random();
             for(int j = 0; j< dates.size();j++){
-                max = 100f;
-                //  value = random.nextFloat()*max;
-                barEntries.add(new BarEntry(hashMap.get(dates.get(j)),j));
+                barEntries.add(new BarEntry(j,garbage.get(dates.get(j))));
+               // Log.e("ashu",barEntries.get(0)+"");
             }
 
-        }catch(ParseException e){
-            e.printStackTrace();
-        }
 
         BarDataSet barDataSet = new BarDataSet(barEntries,"Dates");
+      //  Log.e("ashu",barEntries.get(0)+"");
         BarData barData = new BarData(barDataSet);
         barChart.setData(barData);
-        barChart.setDescription("My First Bar Graph!");
+        barChart.setDescription("Garbage Daily Count!");
 
-    }
-
-    public ArrayList<String> getList(Calendar startDate, Calendar endDate){
-        ArrayList<String> list = new ArrayList<String>();
-        while(startDate.compareTo(endDate)<=0){
-            list.add(getDate(startDate));
-            startDate.add(Calendar.DAY_OF_MONTH,1);
-        }
-        return list;
-    }
-
-    public String getDate(Calendar cld){
-        String curDate = cld.get(Calendar.YEAR) + "/" + (cld.get(Calendar.MONTH) + 1) + "/"
-                +cld.get(Calendar.DAY_OF_MONTH);
-        try{
-            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(curDate);
-            curDate =  new SimpleDateFormat("yyy/MM/dd").format(date);
-        }catch(ParseException e){
-            e.printStackTrace();
-        }
-        return curDate;
     }
 
 }
